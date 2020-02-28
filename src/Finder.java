bad8540393558c49b7dd7db8bfef48dbe57efc78
ashishbg <ashishbg.g@gmail.com>
2020-02-28 15:57:32 +0530

    Added command line support along with CLI help Required arguements 	-d directory to search, 	-t file type to search into, 	-k search key Optional arguement 	-w file name to save search results

--- src/Finder.java
+++ src/Finder.java
@@ -11,53 +11,59 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Finder {
-	
-	static BufferedReader br = null;
-	List<String> filePathList = null;			
-	List<String> searchHitList = null;
-	static HashMap<String, List<String>> searchResult = null;
-	
-	private long startTime = 0, endTime = 0, totalTime = 0;
-	private static long searchHits = 0;
-	private static long memoryUsed = 0;
-	
-	Finder(){		
+
+	static BufferedReader br;
+	List<String> filePathList;
+	List<String> searchHitList;
+	static HashMap<String, List<String>> searchResult;
+
+	private long startTime, endTime, totalTime;
+	private static long searchHits;
+	private long memoryUsed;
+
+	Finder() {
+		br = null;
+		filePathList = null;
+		searchHitList = null;
+		searchResult = null;
+
+		startTime = 0; endTime = 0; totalTime = 0;
+		searchHits = 0; memoryUsed = 0;
 	}
-	
-	public void find(File dir, String type, String key){
-		if(!dir.exists()) {
+
+	public void find(File dir, String type, String key) {
+		if (!dir.exists()) {
 			System.out.println("No such directory found!");
 			return;
-		}		
-
-		filePathList = new ArrayList<String>();			
-		searchHitList = new ArrayList<String>();
-		
-		startTime = System.currentTimeMillis();
-		
-		try {
-			filePathList = Finder.listFilePaths(dir, type);	//getting paths of all files of 'type' in 'dir'
-			searchResult = Finder.searchKeyInFile2(key, filePathList);
-			
-			//Finding memory usage
-			
-	        Runtime runtime = Runtime.getRuntime(); // Get the Java runtime	        
-	        runtime.gc();	// Run the garbage collector
-	        memoryUsed = runtime.totalMemory() - runtime.freeMemory();	// Calculate the used memory
 		}
-		catch(Exception e) {
+
+		filePathList = new ArrayList<String>();
+		searchHitList = new ArrayList<String>();
+
+		startTime = System.currentTimeMillis();
+
+		try {
+			filePathList = Finder.listFilePaths(dir, type); // getting paths of all files of 'type' in 'dir'
+			searchResult = Finder.searchKeyInFile2(key, filePathList);
+
+			// Finding memory usage
+
+			Runtime runtime = Runtime.getRuntime(); // Get the Java runtime
+			runtime.gc(); // Run the garbage collector
+			memoryUsed = runtime.totalMemory() - runtime.freeMemory(); // Calculate the used memory
+		} catch (Exception e) {
 			e.printStackTrace();
-		}		
-		endTime = System.currentTimeMillis();		
+		}
+		endTime = System.currentTimeMillis();
 	}
-	
+
 	/* returns List of file paths of type */
 	private static List<String> listFilePaths(File dir, final String type) {
-		
+
 		List<String> files = new ArrayList<String>();
-		
+
 		String pattern = ".*\\." + type;
-		if(type == null || type.contentEquals("")) {
+		if (type == null || type.contentEquals("")) {
 			pattern = ".*";
 		}
 		for (File f : dir.listFiles()) {
@@ -75,8 +81,8 @@ public class Finder {
 		}
 		return files;
 	}
-	
-	public static HashMap<String, List<String>> searchKeyInFile2(String key, List<String> paths) {
+
+	private static HashMap<String, List<String>> searchKeyInFile2(String key, List<String> paths) {
 		String line = "";
 		searchResult = new HashMap<String, List<String>>();
 		List<String> lines = null;
@@ -89,22 +95,21 @@ public class Finder {
 				/* Searching for key */
 				while ((line = br.readLine()) != null) {
 					if (line.contains(key)) {
-						lines.add(line.replaceAll("\t",""));
+						lines.add(line.replaceAll("\t", ""));
 						searchHits++;
 					}
-				}				
+				}
 				searchResult.put(filePath, lines);
 				lines = null;
 			}
 
-			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
-				if(br != null) {
+				if (br != null) {
 					br.close();
-				}				
+				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -113,41 +118,74 @@ public class Finder {
 		return searchResult;
 	}
 	
+	private static List<String> searchKeyInFile(String key, String path) {
+		String line = "";
+
+		List<String> lines = null;
+
+		try {
+			br = new BufferedReader(new FileReader(path));
+			lines = new ArrayList<String>();
+			/* Searching for key */
+			while ((line = br.readLine()) != null) {
+				if (line.contains(key)) {
+					lines.add(line);
+				} else {
+					lines.add(null);
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lines;
+	}
+
+
 	public void printSearchResult() {
-		
-		if(searchResult == null) {
+
+		if (searchResult == null) {
 			System.out.println("No search search results");
 			return;
 		}
 		ArrayList<String> hits = null;
-		for(Map.Entry file : searchResult.entrySet()){
-					
-			hits = (ArrayList<String>) file.getValue();	
-			
-			if(hits.isEmpty()) {
-				continue;				
-			}
-			else {
+		for (Map.Entry file : searchResult.entrySet()) {
+
+			hits = (ArrayList<String>) file.getValue();
+
+			if (hits.isEmpty()) {
+				continue;
+			} else {
 				System.out.println("\n" + file.getKey() + ": ");
-				for(String s : hits) {
+				for (String s : hits) {
 					System.out.println(s);
 				}
 			}
 			System.out.println("\n");
 		}
 	}
-	
-public void writeSearchResult() {
+
+	public void writeSearchResult(String outFile){
 		
-		if(searchResult == null) {
+
+		if (searchResult == null) {
 			System.out.println("No search search results");
 			return;
 		}
-		BufferedWriter bw = null;
-		ArrayList<String> hits = null;
 		
+		BufferedWriter bw = null;
+		ArrayList<String> hits = null;		
+		
+
 		try {
-			bw = new BufferedWriter(new FileWriter("search_result.txt"));
+			if(outFile == null) {
+				System.out.println("Search results will be saved to file search_result.txt");
+				bw = new BufferedWriter(new FileWriter("search_result.txt"));
+			}
+			else {
+				System.out.println("Search results will be saved to file " + outFile + ".txt");
+				bw = new BufferedWriter(new FileWriter( outFile + ".txt"));
+			}
 			for (Map.Entry file : searchResult.entrySet()) {
 
 				hits = (ArrayList<String>) file.getValue();
@@ -164,55 +202,30 @@ public void writeSearchResult() {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally {
-			if(bw != null) {
+		} finally {
+			if (bw != null) {
 				try {
 					bw.close();
-				}
-				catch(Exception e) {
+				} catch (Exception e) {
 					e.getMessage();
 				}
 			}
 		}
-		
+
 	}
-	
+
 	public void printSearchStats() {
-		long diffTime = endTime - startTime;
+		totalTime = endTime - startTime;
 		System.out.println("==================================================");
-		if(diffTime > 1000) {
-			System.out.println(searchHits + " results in " + (float)(diffTime/1000) + " s");
+		if (totalTime > 1000) {
+			System.out.println(searchHits + " results in " + (float) (totalTime / 1000) + " s");
+		} else if (totalTime >= 0) {
+			System.out.println(searchHits + " results in " + totalTime + " ms");
 		}
-		else if(diffTime >= 0) {
-			System.out.println(searchHits + " results in " + diffTime + " ms");
-		}
-        System.out.println("Used memory: " + memoryUsed + " bytes");
-        System.out.println("Used memory: " + (float)(memoryUsed/(1024*1024)) + " MB");
-	}
+		System.out.println("Used memory: " + memoryUsed + " B" + (float) (memoryUsed / (1024 * 1024)) + " MB");
+		System.out.println("Used memory: " + (float) (memoryUsed / (1024 * 1024)) + " MB");
 
-	public static List<String> searchKeyInFile(String key, String path) {
-		String line = "";
-
-		List<String> lines = null;
-
-		try {
-			br = new BufferedReader(new FileReader(path));
-			lines = new ArrayList<String>();
-			/* Searching for key */
-			while ((line = br.readLine()) != null) {
-				if (line.contains(key)) {
-					lines.add(line);
-				}
-				else {
-					lines.add(null);
-				}
-			}
-			br.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return lines;
+		System.out.println("==================================================");
 	}
 
 	public static void printFileContents(String path) {
@@ -220,7 +233,7 @@ public void writeSearchResult() {
 
 		/* Print all text from file */
 		try {
-			
+
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
 			}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -13,23 +16,100 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
+		String test_path = ".";
+		String test_type = "";
+		String test_key = "";
+		String test_writepath = null;
+		File test_dir = new File(test_path);
+		
 		/* CLI */
 		
+		final Map<String, List<String>> params = new HashMap<>();
 
-		// Test values
-		// String test_path = "D:\\ashish_workspace\\SearchTool\\test_folder_2";
-		// String test_path = "D:/ashish_workspace/SearchTool/test_folder_2";
-		String test_path = "D:/ashish_workspace/";
-		String test_type = "java";
-		String test_key = "for";
-		File test_dir = new File(test_path);
+		List<String> options = null;
+		for (int i = 0; i < args.length; i++) {
+		    final String a = args[i];
+
+		    if (a.charAt(0) == '-') {
+		        if (a.length() < 2) {
+		            System.err.println("Error at argument " + a);
+		            printCliHelp();
+		            return;
+		        }
+
+		        options = new ArrayList<>();
+		        params.put(a.substring(1), options);
+		    }
+		    else if (options != null) {
+		        options.add(a);
+		    }
+		    else {
+		        System.err.println("Illegal parameter usage");
+		        printCliHelp();
+		        return;
+		    }
+		}
+		
+		if(params.isEmpty()) {
+			System.out.println("All parameters ar required!");
+			printCliHelp();
+			return;
+		}
+		test_path = params.get("d").get(0);
+		test_type = params.get("t").get(0);
+		test_key = params.get("k").get(0);
+		test_dir = new File(test_path);
 		
 		
 		Finder finder = new Finder();
 		finder.find(test_dir, test_type, test_key);
-		//finder.printSearchResult();
-		finder.writeSearchResult();
+		finder.printSearchResult();
 		finder.printSearchStats();
 		
+		if (params.containsKey("w")) {
+			System.out.println("w option exists");
+			if (!params.get("w").isEmpty() && params.get("w") !=null) {
+				System.out.println("file name: " + params.get("w").get(0) );
+				finder.writeSearchResult(params.get("w").get(0));
+			}
+			
+		}
+		
+		
+		
+		
+		/*CLI END*/
+		
+		
+
+		/* Test values
+		
+		// String test_path = "D:\\ashish_workspace\\SearchTool\\test_folder_2";
+		// String test_path = "D:/ashish_workspace/";
+		test_path = "D:/ashish_workspace/";
+		test_type = "java";
+		test_key = "for";
+		test_dir = new File(test_path);
+		*/
+		
+		/*	
+		
+		Finder finder = new Finder();
+		finder.find(test_dir, test_type, test_key);
+		//finder.printSearchResult();
+		//finder.writeSearchResult();
+		finder.printSearchStats();
+		*/
+		
+	}
+	
+	private static void printCliHelp() {
+		/*CLI Help*/
+		System.out.println("[required] -d \"dir\" to search; (.) for current directory");
+		System.out.println("[required] -k \"search-key\"");
+		System.out.println("[required] -t \"file-type\"");
+		System.out.println("[optional] -w \"file-name\" to save search results ");
+		System.out.println("Ex: SourceCode -d \"project\" -k \"hello\" -t \"txt\" -- searches for hello in project folder in file of type txt");
+		System.out.println("");
 	}
 }
